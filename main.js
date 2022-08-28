1c52236f930bb88ff4794f71d0560e388ef9295c
Dtsoy77 <111747815+Dtsoy77@users.noreply.github.com>
2022-08-29 01:03:26 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -5,14 +5,18 @@ document.querySelector('button').onclick = getWhats;
 
 function getWhats(){
     let number = document.getElementById('inp_1').value;
+        if(number.length === 12){
+            let url = `https://wa.me/${number}`
+            let urlWhats = document.createElement('a')
+            urlWhats.href = url
+            urlWhats.textContent = 'Перейти в чат'
+            //dataUrlClick.append(urlWhats)
+            urlWhats.click()
+        }else{
+            alert('введите корректные данные')
+        }
 
 
-    let url = `https://wa.me/${number}`
-    let urlWhats = document.createElement('a')
-    urlWhats.href = url
-    urlWhats.textContent = 'Перейти в чат'
-    //dataUrlClick.append(urlWhats)
-    urlWhats.click()
 
 
 
